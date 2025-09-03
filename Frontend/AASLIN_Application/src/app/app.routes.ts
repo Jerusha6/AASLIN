126869bce90b76145f2e04207a40004a1d1f2fec
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-09-03 15:31:48 +0530

    Merge conflict removed

--- Frontend/AASLIN_Application/src/app/app.routes.ts
+++ Frontend/AASLIN_Application/src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { SignUp } from './LoginSignupForms/Components/sign-up/sign-up';
-
-export const routes: Routes = [
-    {path:'signup',component:SignUp}
-=======
 import { Login } from './LoginSignupForms/Components/login/login';
-
 export const routes: Routes = [
+    {path:'signup',component:SignUp},
     {path:'login', component:Login}
->>>>>>> 111a49e561317bf2f1da07fac8b2baff55ce8ff2
 ];
