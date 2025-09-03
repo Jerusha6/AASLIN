111a49e561317bf2f1da07fac8b2baff55ce8ff2
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-09-03 15:22:31 +0530

    Login Form updated

--- Frontend/AASLIN_Application/src/app/LoginSignupForms/Components/login/login.ts
+++ Frontend/AASLIN_Application/src/app/LoginSignupForms/Components/login/login.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-
+form : FormGroup = new FormGroup({
+    username : new FormControl('',[Validators.required]),
+    password : new FormControl('',[Validators.required,Validators.minLength(6)])
+  })
 }
