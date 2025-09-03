e0ce433b13d810793b3091dd2654ffeb40100361
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-09-03 16:21:20 +0530

    Forgot Password Forms Created

--- Frontend/AASLIN_Application/src/app/LoginSignupForms/Components/login/login.ts
+++ Frontend/AASLIN_Application/src/app/LoginSignupForms/Components/login/login.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,11 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private router : Router){
+  }
+signupPage() {
+  this.router.navigateByUrl("/signup");
+}
 form : FormGroup = new FormGroup({
     username : new FormControl('',[Validators.required]),
     password : new FormControl('',[Validators.required,Validators.minLength(6)])
