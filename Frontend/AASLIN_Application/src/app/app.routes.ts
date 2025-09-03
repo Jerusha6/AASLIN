92fa4e6c1674e8c6220cdf837d937d2373993320
mahesh <maheshkalingi99@gmail.com>
2025-09-03 16:22:32 +0530

    updated

--- Frontend/AASLIN_Application/src/app/app.routes.ts
+++ Frontend/AASLIN_Application/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { Login } from './LoginSignupForms/Components/login/login';
 import { SignUp } from './LoginSignupForms/Components/sign-up/sign-up';
+import { Login } from './LoginSignupForms/Components/login/login';
+
 export const routes: Routes = [
     {path:'signup',component:SignUp},
     {path:'login', component:Login}
