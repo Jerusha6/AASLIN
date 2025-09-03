ad73ff1841a618b147f875b018210cc0067d4c0d
mahesh <maheshkalingi99@gmail.com>
2025-09-03 15:25:14 +0530

    Registration form Updated

--- Frontend/AASLIN_Application/src/app/app.routes.ts
+++ Frontend/AASLIN_Application/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SignUp } from './LoginSignupForms/Components/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'signup',component:SignUp}
+];
