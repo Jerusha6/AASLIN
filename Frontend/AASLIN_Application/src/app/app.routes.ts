3945150e383a296a947bd456c3d2c73021f0b5a8
mahesh <maheshkalingi99@gmail.com>
2025-09-03 17:48:41 +0530

    forgot password progress

--- Frontend/AASLIN_Application/src/app/app.routes.ts
+++ Frontend/AASLIN_Application/src/app/app.routes.ts
@@ -1,19 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignUp } from './LoginSignupForms/Components/sign-up/sign-up';
 import { Login } from './LoginSignupForms/Components/login/login';
-<<<<<<< HEAD
 import { ForgotPasswordUpdate } from './LoginSignupForms/Components/forgot-password-update/forgot-password-update';
-export const routes: Routes = [
-    {path:'signup',component:SignUp},
-    {path:'login', component:Login},
-    {path:'UpdateForgotPassword', component:ForgotPasswordUpdate}
-];
-=======
 import { ForgotPassword } from './LoginSignupForms/Components/forgot-password/forgot-password';
-
 export const routes: Routes = [
     {path:'signup',component:SignUp},
     {path:'login', component:Login},
+    {path:'UpdateForgotPassword', component:ForgotPasswordUpdate},
     {path:'forgotPassword', component:ForgotPassword}
 ];
->>>>>>> d2e5d0e0b4fa7ff60416281106207d2c18bc585c
