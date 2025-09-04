c23e86c1089d0eb358d686d6de2c5b48bcedf984
jerusha <jerusharanitamiri8663@gmail.com>
2025-09-04 08:02:35 +0530

    Created OTP Generator class in util

--- Backend/AaslinJobPortal/src/main/java/com/aaslin/JobPortal/utils/OTPGenerator.java
+++ Backend/AaslinJobPortal/src/main/java/com/aaslin/JobPortal/utils/OTPGenerator.java
@@ -1,5 +1,15 @@
 package com.aaslin.JobPortal.utils;
 
-public class OTPGenerator {
+import java.util.Random;
 
+public class OTPGenerator {
+    public String generateOTP(){
+        int size = 6;
+            StringBuilder otp = new StringBuilder();
+            Random random = new Random();
+            for (int iteration = 0; iteration < size; iteration++) {
+                otp.append(random.nextInt(10));
+            }
+            return otp.toString();
+    }
 }
