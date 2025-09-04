9e71eb51eb20e03d00efed434aedb63440278b7a
mahesh <maheshkalingi99@gmail.com>
2025-09-04 17:32:50 +0530

    Career Page in Progress

--- Frontend/AASLIN_Application/src/app/CarreersPage/careers-first-page/careers-first-page.ts
+++ Frontend/AASLIN_Application/src/app/CarreersPage/careers-first-page/careers-first-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MatCardModule, MatCardSubtitle } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-careers-first-page',
-  imports: [ MatCardModule],
+  imports: [ MatCardModule,MatButtonModule],
   templateUrl: './careers-first-page.html',
   styleUrl: './careers-first-page.css'
 })
