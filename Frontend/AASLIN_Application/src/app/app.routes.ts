e9a6ff8502d89dc1ce1b462692da2148cc17a975
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-09-04 11:23:19 +0530

    Job Apply Updated

--- Frontend/AASLIN_Application/src/app/app.routes.ts
+++ Frontend/AASLIN_Application/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { SignUp } from './LoginSignupForms/Components/sign-up/sign-up';
 import { Login } from './LoginSignupForms/Components/login/login';
 import { ForgotPasswordUpdate } from './LoginSignupForms/Components/forgot-password-update/forgot-password-update';
 import { ForgotPassword } from './LoginSignupForms/Components/forgot-password/forgot-password';
+import { Register } from './Registration/Components/register/register';
 export const routes: Routes = [
     {path:'signup',component:SignUp},
     {path:'login', component:Login},
     {path:'UpdateForgotPassword', component:ForgotPasswordUpdate},
-    {path:'forgotPassword', component:ForgotPassword}
+    {path:'forgotPassword', component:ForgotPassword},
+    {path:'register', component:Register}
 ];
