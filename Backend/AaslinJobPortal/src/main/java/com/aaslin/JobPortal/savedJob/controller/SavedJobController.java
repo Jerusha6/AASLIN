ae085e1d9ca3adbc9cc42bde22c2922370ee1e28
Gnaneswara rao <gnani07012004@gmail.com>
2025-09-05 14:17:42 +0530

    Added SavedJob Controller,Repository and Service

---
+++ Backend/AaslinJobPortal/src/main/java/com/aaslin/JobPortal/savedJob/controller/SavedJobController.java
@@ -0,0 +1,36 @@
+package com.aaslin.JobPortal.savedJob.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.aaslin.JobPortal.savedJob.model.SavedJob;
+import com.aaslin.JobPortal.savedJob.service.SavedJobService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/careers/saved-jobs")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequiredArgsConstructor
+public class SavedJobController {
+
+	private final SavedJobService savedJobService;
+	
+	@PostMapping("/{id}")
+	public String saveJob(@PathVariable("id") String id, @RequestParam String email) {
+		savedJobService.saveJob(id, email);
+		return "Job saved Succesfully!";
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteJob(@PathVariable("id") String id, @RequestParam String email) {
+		savedJobService.removeJobByJobPostId(id, email);
+		return "Job removed from Saved List.";
+	}
+	
+	@GetMapping
+	public List<SavedJob> allSavedJobs(@RequestParam String email) {
+		return savedJobService.getAllSavedJobsByEmail(email);
+	}
+}
